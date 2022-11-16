65fcfb76c9019af4d0e92c4800329cff0e2aae55
dunneev <dunneev@gmail.com>
2022-11-15 20:22:18 -0500

    Include documentation

--- app/src/main/java/net/eldun/morso/MorsoGestureListener.kt
+++ app/src/main/java/net/eldun/morso/MorsoGestureListener.kt
@@ -10,12 +10,26 @@ class MorsoGestureListener : GestureDetector.SimpleOnGestureListener() {
 
     var morsoUiState = MorsoUiState()
 
+    /**
+     * Notified when a tap occurs with the down [MotionEvent]
+     * that triggered it. This will be triggered immediately for
+     * every down event. All other events should be preceded by this.
+     *
+     * @param e The down motion event.
+     */
     override fun onDown(e: MotionEvent): Boolean {
         Log.d(TAG, "downMotion detected!")
 
         return true
     }
 
+    /**
+     * Notified when a tap occurs with the up [MotionEvent]
+     * that triggered it.
+     *
+     * @param e The up motion event that completed the first tap
+     * @return true if the event is consumed, else false
+     */
     override fun onSingleTapUp(e: MotionEvent): Boolean {
         Log.d(TAG, "tap detected!")
         morsoUiState.backgroundText.value = "tapped"
