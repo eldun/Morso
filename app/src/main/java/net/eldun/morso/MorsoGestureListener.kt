d96f6b86c6900dccf5660ddf1620de7d818a8e74
dunneev <dunneev@gmail.com>
2022-11-19 19:16:42 -0500

    Recognize all necessary gestures

--- app/src/main/java/net/eldun/morso/MorsoGestureListener.kt
+++ app/src/main/java/net/eldun/morso/MorsoGestureListener.kt
@@ -1,10 +1,11 @@
 package net.eldun.morso
 
-import android.inputmethodservice.InputMethodService
 import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.inputmethod.InputConnection
+import net.eldun.morso.enums.Character
+import net.eldun.morso.enums.MorseSignal
 
 class MorsoGestureListener : GestureDetector.SimpleOnGestureListener() {
 
@@ -33,15 +34,56 @@ class MorsoGestureListener : GestureDetector.SimpleOnGestureListener() {
      */
     override fun onSingleTapUp(e: MotionEvent): Boolean {
         Log.d(TAG, "onSingleTapUp")
-        morsoUiState.backgroundText.value = "tapped"
+        morsoUiState.backgroundText.value.apply { "." }
+
+        updateCandidates(MorseSignal.DOT)
+
         inputConnection.commitText("!", 1)
+
         return true
     }
 
+
+
     fun onHold(e: MotionEvent): Boolean {
 
+        updateCandidates(MorseSignal.DASH)
         Log.d(TAG, "onHold")
         return true
 
+    }
+
+    fun onShortPause(e: MotionEvent): Boolean {
+        Log.d(TAG, "onShortPause")
+        morsoUiState.reset()
+        return true
+    }
+
+    fun onLongPause(e: MotionEvent): Boolean {
+        Log.d(TAG, "onLongPause")
+        inputConnection.commitText(" ", 1)
+
+        return true
+    }
+
+    private fun updateCandidates(signal: MorseSignal) {
+
+        if (signal == MorseSignal.DOT){
+            morsoUiState.currentCandidateText.value = morsoUiState.dotCandidateText.value
+
+            val newCurrent = Character.fromString(morsoUiState.currentCandidateText.value.toString())
+
+            morsoUiState.dotCandidateText.value = Character.getDotChild(newCurrent!!).toString()
+            morsoUiState.dashCandidateText.value = Character.getDashChild(newCurrent!!).toString()
+        }
+
+        else if (signal == MorseSignal.DASH){
+            morsoUiState.currentCandidateText.value = morsoUiState.dashCandidateText.value
+
+            val newCurrent = Character.fromString(morsoUiState.currentCandidateText.value.toString())
+
+            morsoUiState.dotCandidateText.value = Character.getDotChild(newCurrent!!).toString()
+            morsoUiState.dashCandidateText.value = Character.getDashChild(newCurrent!!).toString()
+        }
     }
 }
--- app/src/main/java/net/eldun/morso/MorsoInputView.kt
+++ app/src/main/java/net/eldun/morso/MorsoInputView.kt
@@ -5,6 +5,7 @@ import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.SystemClock
 import android.util.AttributeSet
 import android.util.Log
 import android.view.GestureDetector
@@ -23,7 +24,13 @@ class MorsoInputView @JvmOverloads constructor(
 
     val gestureListener =  MorsoGestureListener()
     private val gestureDetector = GestureDetector(context, gestureListener)
-    private val longPressTimeout: Long = 1500
+    private var downTime: Long = 0
+    private var upTime: Long = 0
+    private val dotTime: Long = 300
+    private val dashTime = 3*dotTime
+    private val signalSpaceTimeout = dotTime
+    private val letterSpaceTimeout: Long = 3*dotTime
+    private val wordSpaceTimeout: Long = 7*dotTime
 
 
     private var backgroundText = "Morso"
@@ -49,6 +56,8 @@ class MorsoInputView @JvmOverloads constructor(
     private var centerY = 100F
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+
         val desiredWidth = 100
         val desiredHeight = getScreenHeight() / 4
 
@@ -56,6 +65,8 @@ class MorsoInputView @JvmOverloads constructor(
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+
 
         //Measure Width
         val width = when (widthMode) {
@@ -70,6 +81,7 @@ class MorsoInputView @JvmOverloads constructor(
             MeasureSpec.AT_MOST -> min(desiredHeight, heightSize)
             else -> desiredHeight
         }
+
 
         //MUST CALL THIS
         setMeasuredDimension(width, height)
@@ -89,24 +101,44 @@ class MorsoInputView @JvmOverloads constructor(
         canvas.drawText(backgroundText, centerX, centerY, paint)
     }
 
-
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
-        // call onHold if an ACTION_UP has not been received in longPressTimeout ms
-        if (event.actionMasked == MotionEvent.ACTION_DOWN) {
-            handler.postDelayed({ gestureListener.onHold(event) }, longPressTimeout)
-        } else if (event.actionMasked == MotionEvent.ACTION_UP) {
-            handler.removeCallbacksAndMessages(null)
-        }
+        val onHoldRunnable = Runnable { gestureListener.onHold(event) }
+        val shortPauseRunnable = Runnable { gestureListener.onShortPause(event) }
+        val longPauseRunnable = Runnable { gestureListener.onLongPause(event) }
 
-        if (gestureDetector.onTouchEvent(event)) {
-            // The event has been consumed by our simple gesture listener
+
+        if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+            downTime = SystemClock.elapsedRealtime()
+
+            // Cancel possible pending runnables
+            handler.removeCallbacksAndMessages(null)
+
+            // Call onHold in dashTime ms
+            handler.postDelayed(onHoldRunnable, dashTime)
+        } else if (event.actionMasked == MotionEvent.ACTION_UP) {
+            // Cancel the pending hold runnable and previous pause runnables
+            handler.removeCallbacksAndMessages(null)
+
+            upTime = SystemClock.elapsedRealtime()
+
+
+            // Listen for all taps with no restrictions (slop, triple-taps, etc. - unlike our gesture detector)
+            val elapsedTime = upTime - downTime
+            if (elapsedTime < dotTime) {
+                gestureListener.onSingleTapUp(event)
+            }
+
+
+            // call timeouts if no input has been received
+            handler.postDelayed(shortPauseRunnable, letterSpaceTimeout)
+            handler.postDelayed(longPauseRunnable, wordSpaceTimeout)
+
             return true
         }
 
-        // TODO: determine what to do with ambiguous signals
-        // maybe add a progress bar for when the input is held
-        return false
+        // It's up to MorsoGestureListener to decide
+        return gestureDetector.onTouchEvent(event)
 
     }
 
